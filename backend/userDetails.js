2ec93121a7c89f48453acdc8d9f577110b47e045
BristiJana <54547239+BristiJana@users.noreply.github.com>
2023-02-09 14:25:12 +0530

    Add files via upload

---
+++ backend/userDetails.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const UserDetailsScehma = new mongoose.Schema(
+  {
+    fname: String,
+    lname: String,
+    email: { type: String, unique: true },
+    password: String,
+  },
+  {
+    collection: "UserInfo",
+  }
+);
+const ProjectDetailsScehma = new mongoose.Schema({
+  name: String,
+  details: String,
+  Success: String,
+  sensors: Array,
+});
+const User = mongoose.model("UserInfo", UserDetailsScehma);
+const Project = mongoose.model("ProjectInfo", ProjectDetailsScehma);
+
+module.exports = {
+  User,
+  Project,
+};
